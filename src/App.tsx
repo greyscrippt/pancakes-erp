bda11ccac8413ced519bd48ccdb2e6e2777feddb
Mongark <mongarkprogrammer@gmail.com>
2023-05-17 16:48:08 -0400

    Added and implemented react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './styles.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>
+  },
+]);
 
 function App() {
   return (
-    <>
-        Hello
-    </>
+    <RouterProvider router={router} />
   )
 }
 
